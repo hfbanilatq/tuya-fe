7f44edc5a366a835c76428fe9b043d56a44b3895
Hector Fabio Banilat Quintero <53194376+hfbanilatq@users.noreply.github.com>
2022-06-01 17:11:00 -0500

    creating initial structure

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
